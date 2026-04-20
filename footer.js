4809a2fcf301ceddb7f5d5e35e8c05b06e263b6d
munux9032 <munux9032@gmail.com>
2026-04-20 20:27:12 +0900

    Create footer.js

---
+++ footer.js
@@ -0,0 +1,8 @@
+document.write (
+  '<div class="footer" style="display: flex; justify-content: space-between; align-items: center;">',
+     '<span>&emsp;<a href="http://www.core.kochi-tech.ac.jp/m_inoue/work/index.html" style="color:#fff; text-decoration: underline;">竕ｪ 繝医ャ繝励∈謌ｻ繧�</a></span>',
+    '<small>',
+      'Copywrite (C) 1999-2022 INOUE Masaaki, TeXClub.',
+    '</small>',
+  '</div>'
+);
