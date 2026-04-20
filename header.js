4b412da7f293b23899d33ea5f3ab76e28c1cf9d3
munux9032 <munux9032@gmail.com>
2026-04-20 20:27:56 +0900

    Create header.js

---
+++ header.js
@@ -0,0 +1,13 @@
+/*
+document.write (
+  '<div class="header">',
+    '<ul>',
+      '<li><a href="index.html"> 繝医ャ繝� </a></li>',
+      '<li><a href="workbook.html"> 繝ｯ繝ｼ繧ｯ繝悶ャ繧ｯ荳隕ｧ </a></li>',
+      '<li><a href="about_math.html"> 謨ｰ蟄ｦ縺ｫ縺､縺�※ </a></li>',
+      '<li><a href="e-learning.html"> e-learning </a></li>',
+      '<li><a href="link.html"> 繝ｪ繝ｳ繧ｯ </a></li>',
+    '</ul>',
+  '</div>'
+);
+*/
